4316e67631b488eb0a3b6aadd72bf23d8881c2ed
Kazue <69866074+Nemo505@users.noreply.github.com>
2023-11-13 11:39:35 +0630

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer text-center">
-    <strong>Copyright &copy; 2023-2025 <a href="https://findixmyanmar.com/en/home-us/">FindixMyanmar.</a>.</strong>
+    <strong>Copyright &copy; 2023-2025 <a href="https://findixmyanmar.com/en/home-us/">Kazue </a>.</strong>
     All rights reserved.
 </footer>
